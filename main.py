6901b7649c9feb72131d74a9e98c280220e2c0f8
Jack Knight <141439028+notjackknight@users.noreply.github.com>
2024-10-13 22:04:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from Employee_Data import Job, Employee, employees, assign_lead
+from Employee_Data import Job, Employee, employees, assign_lead, JobType
 
 
 def greet():
@@ -10,6 +10,18 @@ def greet():
         else "Good evening!"
     )
     print(greeting)
+
+
+def get_job_type():
+    print("\nSelect a job type: ")
+    for job in JobType:
+        print(f"{job.value}. {job.name}")
+    while True:
+        try:
+            choice = int(input("Please select a job type: "))
+            return JobType(choice)
+        except ValueError:
+            print("Invalid choice. Please select an option between 1 and 5.")
 
 
 greet()
@@ -23,9 +35,9 @@ def main():
         print("3. Display employee data")
         print("4. Assign Sales Lead")
         print("5. Exit")
-        print("")
 
-        choice = input("Choose an option (1-5): ")
+        choice = input("\nChoose an option (1-5): ")
+
         if choice == '1':
             name = input("Enter employee name: ")
             if name not in employees:
@@ -37,11 +49,11 @@ def main():
         elif choice == '2':
             name = input("Enter employee name: ")
             if name in employees:
-                job_type = input("Enter job type: ")
+                job_type = get_job_type()
                 sold = input("Was the job sold? (yes/no): ").lower() == 'yes'
                 job = Job(job_type)
-                employees[name].update_performance(job.job_type, sold)
-                print(f"Performance update for {name}")
+                employees[name].update_performance(job_type, sold)
+                print(f"Performance updated for {name}")
             else:
                 print(f"Employee {name} not found.")
 
@@ -49,13 +61,11 @@ def main():
             name = input("Enter employee name to display data: ")
             if name in employees:
                 employees[name].display_info()
-                overall_rate = employees[name].get_overall_success_rate()
-                print(f"Overall success rate for {name}: {overall_rate:.2f}")
             else:
                 print(f"Employee {name} not found.")
 
         elif choice == '4':
-            input("Enter the job type: ")
+            job_type = get_job_type()
             assign_lead(job_type)
 
         elif choice == '5':
@@ -65,10 +75,15 @@ def main():
         else:
             print("Invalid option. Please choose a valid option.")
 
-        go_again = input("Do you want to keep working? (yes/no):").lower()
-        if go_again != 'yes':
-            print("Thank you. Goodbye!")
-            break
+        while True:
+            go_again = input("Do you want to keep working? (Yes/No): ").lower()
+            if go_again == 'no':
+                print("Thank you. Goodbye!")
+                return
+            elif go_again == 'yes':
+                break
+            else:
+                print("You entered an invalid option. Please enter 'Yes' or 'No'.")
 
 
 if __name__ == "__main__":
